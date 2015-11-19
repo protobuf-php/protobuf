a07ec239608c99768693d018ab07766f9fd8bd03
FabioBatSilva <fabio.bat.silva@gmail.com>
2015-11-18 22:49:36 -0500

    update travis config

---
+++ tests/travis/install-protobuf.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/protobuf-$PROTOBUF_VERSION.tar.gz
+
+tar xf protobuf-$PROTOBUF_VERSION.tar.gz
+
+cd protobuf-$PROTOBUF_VERSION && ./configure --prefix=$HOME/protobuf && make && make install
+
+export PATH=$PATH:$HOME/protobuf/bin
