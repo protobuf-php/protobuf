cb6a18d770b6a802b738b5368631e2a5ad78de3e
FabioBatSilva <fabio.bat.silva@gmail.com>
2015-11-20 00:54:49 +0000

    Cache protobuf instalation

--- tests/travis/install-protobuf.sh
+++ tests/travis/install-protobuf.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -e
 
+if [ -d "$HOME/protobuf/lib" ]; then
+  echo 'Using cached instalation.';
+  exit 0;
+fi
+
 wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/protobuf-$PROTOBUF_VERSION.tar.gz
 
 tar xf protobuf-$PROTOBUF_VERSION.tar.gz
 
 cd protobuf-$PROTOBUF_VERSION && ./configure --prefix=$HOME/protobuf && make && make install
 
-export PATH=$PATH:$HOME/protobuf/bin/
