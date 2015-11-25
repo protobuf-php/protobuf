96d10c882a4fd2f1b1418fd203538ce1923f538a
FabioBatSilva <fabio.bat.silva@gmail.com>
2015-11-24 21:02:27 -0500

    add Extension Message interface

---
+++ src/ExtensionMessage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Protobuf;
+
+use Protobuf\ComputeSizeContext;
+use Protobuf\Configuration;
+use Protobuf\WriteContext;
+use Protobuf\ReadContext;
+
+/**
+ * Interface that extension messages implement
+ *
+ * @author Fabio B. Silva <fabio.bat.silva@gmail.com>
+ */
+interface ExtensionMessage
+{
+
+}
